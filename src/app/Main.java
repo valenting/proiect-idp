504b66a4c466f9e9c78160867dff37e2797f5b4c
Emma Mirica <emma.mirica@gmail.com>
2012-03-18 20:56:48 +0200

    Gui start

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,9 +1,13 @@
 package app;
 
+import gui.Gui;
+
 public class Main {
 
 	public static void main(String []args) {
 		System.out.println("hello from main");
 		// TODO
+		Mediator m = new Mediator();
+		new Gui(m);
 	}
 }
--- src/gui/Gui.java
+++ src/gui/Gui.java
@@ -1,5 +1,18 @@
 package gui;
 
-public class Gui {
+import java.awt.Dimension;
+import javax.swing.JFrame;
 
+import app.Mediator;
+
+public class Gui extends JFrame{
+	private static final long serialVersionUID = 1L;
+	Mediator med = null;
+	public Gui(Mediator m) {
+		super("Chat");
+		this.med = m;
+		setLocationRelativeTo(null);
+		setSize(new Dimension(1000, 600));
+		setVisible(true);
+	}
 }
